196b5b2146781ef8c00acf8b2ac66bf659a27abc
alexeyShuncko <shuncko.a@yandex.ru>
2022-08-16 18:45:10 +0300

    Ускорение

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
   const [arr, setArr] = useState([])
 
 
-  let x = 200 
-  let y = 40
+  let x = 200
+  let y = 60
   let angle = 0
 
   const speed = 2 // время перемещения и поворота в секундах
-  const ANG = 30 * Math.PI/180 // угол поворота
+  const ANG = 30 * Math.PI / 180 // угол поворота
   let x1 = 540
   let y1 = 20
 
@@ -28,25 +28,26 @@ function App() {
 
 
 
-  function moveTo(x, x1, y, y1, speed) {
+  function moveTo(x1, y1, speed) {
 
     const start = new Date()
 
     let dx = (x1 - x) / speed / 60
     let dy = (y1 - y) / speed / 60
+
     function draw(timestamp) {
 
       const end = new Date()
       let diff = end - start
-     
-      setArr([...arr,{time: diff, coords: [Math.round(x), Math.round(y)] }])
+
+      setArr([...arr, { time: diff, coords: [Math.round(x), Math.round(y)] }])
 
 
       const canvas = document.getElementById('elSquare')
       const ctx = canvas.getContext('2d')
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      
-      x += dx;
+
+      x += dx
       y += dy;
       square(ctx, x, y)
       if (Math.round(x) <= x1) {
@@ -57,35 +58,117 @@ function App() {
   }
 
 
-  function turnTo(x, y, speed, angle) {
+  function turnTo(speed, angle) {
 
     const start = new Date()
-    let dAngle = ANG/speed / 60
+    let dAngle = ANG / speed / 60
 
     function turn() {
 
       const end = new Date()
       let diff = end - start
-      setArr([...arr,{time: diff, coords: [angle.toFixed(2)] }])
-      
-       const canvas = document.getElementById('elSquare')
-       const ctx = canvas.getContext('2d')
-     
+      setArr([...arr, { time: diff, coords: [Math.round(angle / (Math.PI / 180))] }])
+
+      const canvas = document.getElementById('elSquare')
+      const ctx = canvas.getContext('2d')
+
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      
+
       angle += dAngle
       ctx.rotate(dAngle);
       square(ctx, x, y)
-    
-       if (angle.toFixed(2) <= ANG.toFixed(2)) {
-         requestAnimationFrame(turn)
-       }
-       else {
+
+      if (angle.toFixed(2) <= ANG.toFixed(2)) {
+        requestAnimationFrame(turn)
+      }
+      else {
         ctx.rotate(-ANG);
-       }
+      }
     }
     requestAnimationFrame(turn)
   }
+
+  let boost  = 2*(x1-x)/Math.pow(speed, 2)
+
+  function animate({ timing, draw, duration }) {
+
+    let start = performance.now()
+    const st = new Date()
+
+   
+    requestAnimationFrame(function animate(time) {
+     
+      let timeFraction = (time - start) / duration
+      if (timeFraction > 1) timeFraction = 1
+
+      let progress = timing(timeFraction)
+
+    
+      let dx = (boost * Math.pow(, 2))/2
+      x += dx
+      draw(progress, st)
+
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate)
+      }
+
+    });
+  }
+
+
+  function draw(progress, start) {
+    const canvas = document.getElementById('elSquare')
+    const ctx = canvas.getContext('2d')
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const end = new Date()
+    let diff = end - start
+
+    setArr([...arr, { time: diff, coords: [Math.round(x), Math.round(y)] }])
+    square(ctx, x, y)
+  }
+
+
+  function timing(timeFraction) {
+    return timeFraction
+  }
+
+
+
+  // Анимация торможения
+
+  function animateUp({ timing, draw, duration }) {
+
+    let start = performance.now()
+    const st = new Date()
+
+    requestAnimationFrame(function animate(time) {
+     
+      let timeFraction = (time - start) / duration
+      if (timeFraction > 1) timeFraction = 1
+
+      function makeEaseOut(timing) {
+        return function(timeFraction) {
+          return 1 - timing(1 - timeFraction);
+        }
+      }
+
+      let qqq = makeEaseOut(timing)
+      let progress = qqq(timeFraction)
+
+     console.log(progress);
+      let dx = progress * (x1-x)/speed
+      
+      x += dx
+      draw(progress, st)
+
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate)
+      }
+
+    });
+  }
+
+
 
 
 
@@ -101,24 +184,25 @@ function App() {
   return (
     <div className="App" style={{ display: 'flex' }}>
 
-      <canvas id='elSquare' width={800} height={400}>
+      <canvas id='elSquare' width={700} height={400}>
 
       </canvas>
-      <div style={{display: 'flex', flexDirection: 'column'}}>
-        <button onClick={() => moveTo(x, x1, y, y1, speed)}>Переместить</button>
-        <button onClick={()=> turnTo(x,y, speed, angle)}>Повернуть</button>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <button onClick={() => moveTo(x1, y1, speed)}>Переместить</button>
+        <button onClick={() => turnTo(speed, angle)}>Повернуть</button>
+        <button onClick={() => animate({ timing, draw, duration: speed*1000 })}>Ускорение</button>
       </div>
 
       <ul>
-        Массивы значений, для промежутка времени.
+        <span className="nameList">Массивы значений, для промежутка времени.</span>
         {
           arr.map((a, i) => (
-            <li key={i}> Время:{a.time}, 
-            {
-            a.coords.length > 1 
-            ? <span>координаты[{a.coords[0]},{a.coords[1]}]</span>
-            : <span>угол поворота:[{a.coords[0]}]</span>
-            }
+            <li key={i}> Время: {a.time} мс,
+              {
+                a.coords.length > 1
+                  ? <span>координаты [{a.coords[0]}, {a.coords[1]}]</span>
+                  : <span>угол поворота [{a.coords[0]}°]</span>
+              }
             </li>
           ))
         }
