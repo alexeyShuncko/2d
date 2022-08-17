ee6a62d867a31baa89b118b0af7d5bfd38d0c2e4
alexeyShuncko <shuncko.a@yandex.ru>
2022-08-17 17:37:15 +0300

    Визуальная часть

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
 import Move from './components/Move';
+import { Box, Stack } from '@mui/material';
 
 function App() {
 
-
   const [arr, setArr] = useState([])
-
+  const [speed, setSpeed] = useState(2) // время перемещения и поворота в секундах
+  const [ANG, setANG] = useState(30) // угол поворота
+  const [NewX, setNewX] = useState(540)
+  const [NewY, setNewY] = useState(100)
 
   let x = 200
   let y = 60
   let angle = 0
 
-  const speed = 2 // время перемещения и поворота в секундах
-  const ANG = 30 * Math.PI / 180 // угол поворота
-  let x1 = 540 
-  let y1 = 20
 
+  // Прямоугольник
   function square(ctx, x, y) {
     ctx.beginPath();
     ctx.rect(x, y, 100, 120);
@@ -26,14 +27,15 @@ function App() {
   }
 
 
-// Перемещение квадрата
+  ///////// Набор функций согласно тестовому заданию
 
-  function moveTo(x1, y1, speed) {
+  // Перемещение прямоугольника
+  function moveTo(NewX, NewY, speed) {
 
     const start = new Date()
 
-    let dx = (x1 - x) / speed / 60
-    let dy = (y1 - y) / speed / 60
+    let dx = (NewX - x) / speed / 60
+    let dy = (NewY - y) / speed / 60
 
     function draw() {
 
@@ -42,7 +44,6 @@ function App() {
 
       setArr([...arr, { time: diff, coords: [Math.round(x), Math.round(y)] }])
 
-
       const canvas = document.getElementById('elSquare')
       const ctx = canvas.getContext('2d')
       ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -50,21 +51,22 @@ function App() {
       x += dx
       y += dy;
       square(ctx, x, y)
-      if (Math.round(x) <= x1) {
+
+      if (NewX < 200 ? (Math.round(x) >= NewX) : (Math.round(x) <= NewX)) {
         requestAnimationFrame(draw)
       }
     }
     requestAnimationFrame(draw)
   }
 
-// Поворот квадрата 
+  // Поворот прямоугольника 
   function turnTo(speed, angle) {
 
     const start = new Date()
-    let dAngle = ANG / speed / 60
+    let dAngle = (ANG * Math.PI / 180) / speed / 60
 
     function turn() {
-
+      angle += dAngle
       const end = new Date()
       let diff = end - start
       setArr([...arr, { time: diff, coords: [Math.round(angle / (Math.PI / 180))] }])
@@ -73,46 +75,51 @@ function App() {
       const ctx = canvas.getContext('2d')
 
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      angle += dAngle
+     
       ctx.rotate(dAngle);
       square(ctx, x, y)
+      
 
-      if (angle.toFixed(2) <= ANG.toFixed(2)) {
+      if (angle < ANG * Math.PI / 180) {
         requestAnimationFrame(turn)
       }
+     
       else {
-        ctx.rotate(-ANG);
+        ctx.rotate(-(ANG * Math.PI / 180));
       }
     }
     requestAnimationFrame(turn)
   }
 
-// Ускорение и торможение квадрата по оси X
-
+  // Ускорение и торможение прямоугольника
   function animate({ timing, draw, duration, flag }) {
-
 
     let start = performance.now()
     const st = new Date()
 
-    let boost  = 2*(x1-x)/Math.pow(speed, 2)
+    let boost = 2 * (NewX - x) / Math.pow(speed, 2)
+    let boostY = 2 * (NewY - y) / Math.pow(speed, 2)
+
     requestAnimationFrame(function animate(time) {
-     
+
       let timeFraction = (time - start) / duration
       if (timeFraction > 1) timeFraction = 1
       if (timeFraction < 0) timeFraction = 0
 
       let progress = timing(timeFraction)
 
-    if (flag === 'Ускорение') {
-      let dx = (boost * Math.pow(speed * progress, 2))/2 
-      x = dx + 200
-    }
-   
-      else if (flag === 'Торможение') {
-        let dx = (boost * Math.pow(speed * (1-progress), 2))/2 
-        x = x1 - dx
+      if (flag === 'УСКОРЕНИЕ') {
+        let dx = (boost * Math.pow(speed * progress, 2)) / 2
+        let dy = (boostY * Math.pow(speed * progress, 2)) / 2
+        x = dx + 200
+        y = dy + 60
+      }
+
+      else if (flag === 'ТОРМОЖЕНИЕ') {
+        let dx = (boost * Math.pow(speed * (1 - progress), 2)) / 2
+        let dy = (boostY * Math.pow(speed * (1 - progress), 2)) / 2
+        x = NewX - dx
+        y = NewY - dy
       }
       draw(progress, st)
 
@@ -123,7 +130,10 @@ function App() {
     });
   }
 
-// Функция отрисовки квадрата при торможении, ускорении
+  //////////////
+
+
+  // Функция отрисовки прямоугольника при торможении, ускорении
   function draw(progress, start) {
     const canvas = document.getElementById('elSquare')
     const ctx = canvas.getContext('2d')
@@ -134,7 +144,7 @@ function App() {
     setArr([...arr, { time: diff, coords: [Math.round(x), Math.round(y)] }])
     square(ctx, x, y)
   }
-// Функция анимации (линейная)
+  // Функция анимации (линейная)
   function timing(timeFraction) {
     return timeFraction
   }
@@ -148,31 +158,50 @@ function App() {
 
 
   return (
-    <div className="App" style={{ display: 'flex' }}>
+    <div className="App" >
 
-      <canvas id='elSquare' width={700} height={400}>
-
-      </canvas>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <button onClick={() => moveTo(x1, y1, speed)}>Переместить</button>
-        <Move />
-        <button onClick={() => turnTo(speed, angle)}>Повернуть</button>
-        <button onClick={(e) => animate({ timing, draw, duration: speed*1000, flag: e.target.innerHTML })}>
-          Ускорение</button>
-         <button onClick={(e) => animate({ timing, draw, duration: speed*1000, flag: e.target.innerHTML })}>
-          Торможение</button>
+      <div>
+        <div className='title'>Поле canvas размером 400px на 700px.</div>
+        <div className='title'>Координаты левого угла прямоугольника x: 200px, y: 60px.</div>
+        <canvas id='elSquare' width={700} height={400}>Канвас не работает ....</canvas>
       </div>
 
+
+      <Box>
+        <Stack spacing={1} direction="column" style={{ flex: 1 }}>
+          <Button variant="contained" onClick={() => moveTo(NewX, NewY, speed)}>Переместить</Button >
+
+          <Button variant="contained" onClick={() => turnTo(speed, angle)}>Повернуть</Button >
+          <Button variant="contained"
+            onClick={(e) => animate({ timing, draw, duration: speed * 1000, flag: e.target.innerText })}>
+            Ускорение</Button >
+          <Button variant="contained"
+            onClick={(e) => animate({ timing, draw, duration: speed * 1000, flag: e.target.innerText })}>
+            Торможение</Button >
+        </Stack>
+        <Move
+          speed={speed}
+          setSpeed={setSpeed}
+          NewX={NewX}
+          setNewX={setNewX}
+          NewY={NewY}
+          setNewY={setNewY}
+          ANG={ANG}
+          setANG={setANG}
+        />
+      </Box>
+
+
       <ul>
-        <span className="nameList">Массивы значений, для промежутка времени.</span>
+        <div className="nameList">Массивы значений, </div>
+        <div className="nameList">для промежутка времени.</div>
         {
           arr.map((a, i) => (
-            <li key={i}> Время: {a.time} мс,
-              {
-                a.coords.length > 1
-                  ? <span>координаты [{a.coords[0]}, {a.coords[1]}]</span>
-                  : <span>угол поворота [{a.coords[0]}°]</span>
-              }
+            <li key={i}> Время: {a.time} мс, {
+              a.coords.length > 1
+                ? <span>координаты [{a.coords[0]}, {a.coords[1]}]</span>
+                : <span>угол поворота [{a.coords[0]}°]</span>
+            }
             </li>
           ))
         }
--- src/components/Move.js
+++ src/components/Move.js
@@ -1,11 +1,40 @@
-import React from "react";
+import { Box } from "@mui/material";
 
-const Move =(props)=> {
-  
+
+const Move = (props) => {
+
+
   return (
-    <div>
-      Инпуты
-    </div>
-)
+
+    <Box sx={{ mt: 3, display: 'flex', flexDirection: 'column' }}>
+      <div className="data">Изменение входных данных.</div>
+      <label>Время перемещения в сек:</label>
+      <input
+        type="number"
+        onChange={(e) => props.setSpeed(e.target.value)}
+        value={props.speed}
+      />
+      <label>Координата по оси X:</label>
+      <input
+        type="number"
+        onChange={(e) => props.setNewX(e.target.value)}
+        value={props.NewX}
+      />
+      <label>Координата по оси Y:</label>
+      <input
+        type="number"
+        onChange={(e) => props.setNewY(e.target.value)}
+        value={props.NewY}
+      />
+      <label>Угол поворота в градусах:</label>
+      <input
+        type="number"
+        onChange={(e) => props.setANG(e.target.value)}
+        value={props.ANG}
+      />
+    </Box >
+
+
+  )
 }
 export default Move
